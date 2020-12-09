cbb5c9be57bb8d2031208220e4c0131f443cf91d
Eduardo Cavalcante <42215395+EPEduardoPaixao@users.noreply.github.com>
2020-12-08 22:50:48 -0300

    Update index.js

--- BeTheHero/bethehero/src/pages/cont-hook/index.js
+++ BeTheHero/bethehero/src/pages/cont-hook/index.js
@@ -31,6 +31,7 @@ import {
   Avatar,
   Button,
   Card,
+  Paper,
   CardHeader,
   CardContent,
   CardActions,
@@ -50,43 +51,28 @@ const Profile: React.FC = () => {
       <HeaderProfile />
       <DrawerProfile />
       <main className={classes.main}>
+        {/* --------------Begin Card----------------- */}
         <div className={classes.cardProfile}>
           <Card>
             <CardContent>
-              <div
-                className={classes.divAvatar}
-                style={{
-                  display: 'flex',
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                  margin: 20,
-                }}
-              >
+              {/* -----------Begin Avatar--------------- */}
+              <div className={classes.divAvatar}>
                 <Avatar className={classes.avatar} />
                 <Typography style={{ fontWeight: 'bold', marginLeft: 20 }}>
                   Nome da Pessoa Física
                 </Typography>
               </div>
-
+              {/* -----------End Avatar--------------- */}
               <Divider />
-
-              <div
-                style={{
-                  fontSize: 12,
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  flexDirection: 'column',
-                  marginBottom: 20,
-                }}
-              >
+              {/* -----------Begin Razão Social de CNPJ--------------- */}
+              <div className={classes.divPj}>
                 <Typography>RAZÃO SOCIAL EMPRESA</Typography>
                 <Typography>CNPJ</Typography>
               </div>
+              {/* -----------End Razão Social de CNPJ--------------- */}
             </CardContent>
-            <CardActions
-              style={{ display: 'flex', justifyContent: 'space-around' }}
-            >
+            {/* -----------------Begin Botões------------------ */}
+            <CardActions className={classes.divButton}>
               <Typography className={classes.linkButton}>
                 Editar Perfil
               </Typography>
@@ -94,238 +80,234 @@ const Profile: React.FC = () => {
                 Alterar Senha
               </Typography>
             </CardActions>
+             {/* -----------------End Botões------------------ */}
           </Card>
         </div>
-        {/* --------Final-Cardo------------- */}
-
-        <div className={classes.formProfile}>
-          <div>
-            <TextField
-              size="small"
-              id="standard-full-width"
-              placeholder="Razão Social"
-              style={{ marginTop: '1%' }}
-              fullWidth
-              margin="normal"
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <Business fontSize="small" />
-                  </InputAdornment>
-                ),
-              }}
-            />
-            <TextField
-              size="small"
-              id="standard-full-width"
-              placeholder="CNPJ"
-              style={{ marginTop: '1%' }}
-              fullWidth
-              margin="normal"
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <VerifiedUser fontSize="small" />
-                  </InputAdornment>
-                ),
-              }}
-            />
-            <TextField
-              size="small"
-              id="standard-full-width"
-              placeholder="Nome Fantasia"
-              style={{ marginTop: '1%' }}
-              fullWidth
-              margin="normal"
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <Apartment fontSize="small" />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </div>
-          <div className={classes.formRow}>
-            <TextField
-              size="small"
-              id="standard-full-width"
-              placeholder="CPF"
-              style={{ margin: '0' }}
-              fullWidth
-              margin="normal"
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <VerifiedUser fontSize="small" />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </div>
-          <div className={classes.formRow}>
-            <TextField
-              size="small"
-              id="standard-full-width"
-              placeholder="RG"
-              fullWidth
-              style={{ marginRight: 10 }}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <VerifiedUser fontSize="small" />
-                  </InputAdornment>
-                ),
-              }}
-            />
-            <TextField
-              size="small"
-              id="standard-full-width"
-              placeholder="Órgão Emissor"
-              fullWidth
-              style={{ margin: 0 }}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <VerifiedUser fontSize="small" />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </div>
-          <div className={classes.formRow}>
-            <TextField
-              size="small"
-              id="standard-full-width"
-              placeholder="Data de Emissão"
-              fullWidth
-              style={{ marginRight: 10 }}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <VerifiedUser fontSize="small" />
-                  </InputAdornment>
-                ),
-              }}
-            />
-            <TextField
-              size="small"
-              id="standard-full-width"
-              placeholder="Telefone"
-              fullWidth
-              style={{ margin: '0 0 0 0' }}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <VerifiedUser fontSize="small" />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </div>
-          <div>
-            <TextField
-              size="small"
-              id="standard-full-width"
-              placeholder="Profissão"
-              style={{ marginTop: '1%' }}
-              fullWidth
-              margin="normal"
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <Business fontSize="small" />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </div>
-          <div>
-            <h2 style={{ textAlign: 'center', margin: '2% 0' }}>
-              Endereço da Empresa
-            </h2>
-            <TextField
-              disabled
-              size="small"
-              id="outlined-uncontrolled"
-              label="Tipo Logradouro"
-              fullWidth
-              style={{ marginTop: '0' }}
-              defaultValue=" "
-              variant="outlined"
-            />
-            <TextField
-              disabled
-              size="small"
-              id="outlined-uncontrolled"
-              label="Logradouro"
-              fullWidth
-              style={{ marginTop: '1%' }}
-              defaultValue=" "
-              variant="outlined"
-            />
-            <TextField
-              disabled
-              size="small"
-              id="outlined-uncontrolled"
-              label="bairro"
-              fullWidth
-              style={{ marginTop: '1%' }}
-              defaultValue=" "
-              variant="outlined"
-            />
-            <TextField
-              disabled
-              size="small"
-              id="outlined-uncontrolled"
-              label="Complemento"
-              fullWidth
-              style={{ marginTop: '1%' }}
-              defaultValue=" "
-              variant="outlined"
-            />
-          </div>
-          <div className={classes.formRow}>
-            <TextField
-              disabled
-              size="small"
-              id="outlined-uncontrolled"
-              label="Cidade"
-              fullWidth
-              defaultValue=" "
-              variant="outlined"
-            />
-            <TextField
-              disabled
-              size="small"
-              id="outlined-uncontrolled"
-              label="UF"
-              style={{ margin: '0 1% 0 1%' }}
-              defaultValue=" "
-              variant="outlined"
-            />
-            <TextField
-              disabled
-              size="small"
-              id="outlined-uncontrolled"
-              label="CEP"
-              fullWidth
-              defaultValue=" "
-              variant="outlined"
-            />
-          </div>
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              marginTop: 20,
-              marginBottom: 20,
-            }}
-          >
-            <Button type="submit" variant="contained" color="primary">
-              Salvar
-            </Button>
-          </div>
+        {/* --------End Card------------- */}
+         {/* ------------Begin Form------------------ */}
+        <div style={{marginTop:15}}>
+         <Paper elevation={3} className={classes.formProfile}>
+            <div>
+              <TextField
+                size="small"
+                id="standard-full-width"
+                placeholder="Razão Social"
+                style={{ margin: 0 }}
+                fullWidth
+                margin="normal"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <Business fontSize="small" />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <TextField
+                size="small"
+                id="standard-full-width"
+                placeholder="CNPJ"
+                style={{ margin: 0 }}
+                fullWidth
+                margin="normal"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <VerifiedUser fontSize="small" />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <TextField
+                size="small"
+                id="standard-full-width"
+                placeholder="Nome Fantasia"
+                style={{ margin: 0 }}
+                fullWidth
+                margin="normal"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <Apartment fontSize="small" />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <TextField
+                size="small"
+                id="standard-full-width"
+                placeholder="CPF"
+                style={{ margin: 0 }}
+                fullWidth
+                margin="normal"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <VerifiedUser fontSize="small" />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </div>
+            <div className={classes.formRow}>
+              <TextField
+                size="small"
+                id="standard-full-width"
+                placeholder="RG"
+                fullWidth
+                style={{ marginRight: 10 }}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <VerifiedUser fontSize="small" />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <TextField
+                size="small"
+                id="standard-full-width"
+                placeholder="Órgão Emissor"
+                fullWidth
+                style={{ margin: 0 }}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <VerifiedUser fontSize="small" />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </div>
+            <div className={classes.formRow}>
+              <TextField
+                size="small"
+                id="standard-full-width"
+                placeholder="Data de Emissão"
+                fullWidth
+                style={{ marginRight: 10 }}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <VerifiedUser fontSize="small" />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <TextField
+                size="small"
+                id="standard-full-width"
+                placeholder="Telefone"
+                fullWidth
+                style={{ margin: 0 }}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <VerifiedUser fontSize="small" />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </div>
+            <div>
+              <TextField
+                size="small"
+                id="standard-full-width"
+                placeholder="Profissão"
+                style={{ margin: 0 }}
+                fullWidth
+                margin="normal"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <Business fontSize="small" />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </div>
+            <div>
+              <h2 style={{ textAlign: 'center', margin: '2% 0' }}>
+                Endereço da Empresa
+              </h2>
+              <TextField
+                disabled
+                size="small"
+                id="outlined-uncontrolled"
+                label="Tipo Logradouro"
+                fullWidth
+                style={{ marginTop: '0' }}
+                defaultValue=" "
+                variant="outlined"
+              />
+              <TextField
+                disabled
+                size="small"
+                id="outlined-uncontrolled"
+                label="Logradouro"
+                fullWidth
+                style={{ marginTop: '1%' }}
+                defaultValue=" "
+                variant="outlined"
+              />
+              <TextField
+                disabled
+                size="small"
+                id="outlined-uncontrolled"
+                label="bairro"
+                fullWidth
+                style={{ marginTop: '1%' }}
+                defaultValue=" "
+                variant="outlined"
+              />
+              <TextField
+                disabled
+                size="small"
+                id="outlined-uncontrolled"
+                label="Complemento"
+                fullWidth
+                style={{ marginTop: '1%' }}
+                defaultValue=" "
+                variant="outlined"
+              />
+            </div>
+            <div className={classes.formRow} style={{marginTop: 10}}>
+              <TextField
+                disabled
+                size="small"
+                id="outlined-uncontrolled"
+                label="Cidade"
+                style={{ marginRight: 8 }}
+                fullWidth
+                defaultValue=" "
+                variant="outlined"
+              />
+              <TextField
+                disabled
+                size="small"
+                id="outlined-uncontrolled"
+                label="UF"
+                style={{ margin: 0 }}
+                defaultValue=" "
+                variant="outlined"
+              />
+              <TextField
+                disabled
+                size="small"
+                id="outlined-uncontrolled"
+                label="CEP"
+                style={{ marginLeft: 8 }}
+                fullWidth
+                defaultValue=" "
+                variant="outlined"
+              />
+            </div>
+            <div className={classes.formButton}>
+              <Button type="submit" variant="contained" color="primary">
+                Salvar
+              </Button>
+            </div>
+          </Paper>
         </div>
       </main>
     </div>
@@ -341,6 +323,7 @@ const useStyles = makeStyles((theme: Theme) => ({
 
   root: {
     display: 'flex',
+    
   },
   main: {
     display: 'flex',
@@ -355,10 +338,35 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
 
   // ----------------Card---------------
-  divAvatar: {},
-
   cardProfile: {
     marginTop: 16,
+  },
+
+  avatar: {
+    cursor: 'pointer',
+    width: theme.spacing(10),
+    height: theme.spacing(10),
+  },
+
+  divAvatar: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: 20,
+  },
+  divPj:{
+    fontSize: 12,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    marginBottom: 15,
+    marginTop: 8,
+
+  },
+
+  divButton:{
+    display: 'flex', justifyContent: 'space-around'
   },
 
   linkButton: {
@@ -371,32 +379,20 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
 
   // --------Formulario-----------------
-
+  
+  formProfile: {
+    padding: 20,
+  },  
   formRow: {
     display: 'flex',
     alignItems: 'center',
-    marginTop: '1%',
+    margin: 0,
   },
-
-  formProfile: {
-    backgroundColor: '#fff',
-    // width: '50%',
-    padding: 10,
-    // margin: '1%',
-    borderRadius: '8px',
-    border: '1px solid #ccc',
-    // height: '74vh',
-  },
-
-  // ----------Avatar-----------------------------
-  avatar: {
-    cursor: 'pointer',
-    width: theme.spacing(10),
-    height: theme.spacing(10),
-  },
-
-  title: {
-    fontSize: 14,
+  formButton:{
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: 20,
+    marginBottom: 0,
   },
 }));
 
